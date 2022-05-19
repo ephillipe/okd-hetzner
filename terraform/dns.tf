250828bf8b0c468e5d0c22df1816b05137a156cd
gjpin <3874515+gjpin@users.noreply.github.com>
2022-05-19 19:56:21 +0100

    dns: fix resource vars

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,21 +5,21 @@
 resource "cloudflare_record" "dns_a_api" {
   zone_id = var.cloudflare_dns_zone_id
   name    = "api.${var.cluster_name}.${base_domain}"
-  value   = hcloud_load_balancer.control_plane.ipv4
+  value   = "${hcloud_load_balancer.control_plane.ipv4}"
   type    = "A"
   ttl     = 120
 }
 
 resource "hcloud_rdns" "api" {
-  load_balancer_id = hcloud_load_balancer.control_plane.id
-  ip_address       = hcloud_load_balancer.control_plane.ipv4
+  load_balancer_id = "${hcloud_load_balancer.control_plane.id}"
+  ip_address       = "${hcloud_load_balancer.control_plane.ipv4}"
   dns_ptr          = "api.${var.cluster_name}.${base_domain}"
 }
 
 resource "cloudflare_record" "dns_a_api_int" {
   zone_id = var.cloudflare_dns_zone_id
   name    = "api-int.${var.cluster_name}.${base_domain}"
-  value   = hcloud_load_balancer.control_plane.ipv4
+  value   = "${hcloud_load_balancer.control_plane.ipv4}"
   type    = "A"
   ttl     = 120
 }
@@ -34,7 +34,7 @@ resource "hcloud_rdns" "api_int" {
 resource "cloudflare_record" "dns_a_apps_wc" {
   zone_id = var.cloudflare_dns_zone_id
   name    = "*.apps.${var.cluster_name}.${base_domain}"
-  value   = hcloud_load_balancer.workers.ipv4
+  value   = "${hcloud_load_balancer.workers.ipv4}"
   type    = "A"
   ttl     = 120
 }
@@ -43,7 +43,7 @@ resource "cloudflare_record" "dns_a_apps_wc" {
 resource "cloudflare_record" "dns_a_bootstrap" {
   zone_id = var.cloudflare_dns_zone_id
   name    = "bootstrap.${var.cluster_name}.${base_domain}"
-  value   = hcloud_server.okd_bootstrap.ipv4_address
+  value   = "${hcloud_server.okd_bootstrap.ipv4_address}"
   type    = "A"
   ttl     = 120
 }
@@ -60,7 +60,7 @@ resource "cloudflare_record" "dns_a_control_plane" {
 
   zone_id = var.cloudflare_dns_zone_id
   name    = "okd-control-${count.index}.${var.cluster_name}.${base_domain}"
-  value   = "hcloud_server.okd_control_plane.${count.index}.ipv4_address"
+  value   = "${hcloud_server.okd_control_plane[count.index].ipv4_address}"
   type    = "A"
   ttl     = 120
 }
@@ -68,8 +68,8 @@ resource "cloudflare_record" "dns_a_control_plane" {
 resource "hcloud_rdns" "control_plane" {
   count = var.num_okd_control_plane
 
-  load_balancer_id = "hcloud_server.okd_bootstrap.${count.index}.id"
-  ip_address       = "hcloud_server.okd_bootstrap.${count.index}.ipv4_address"
+  load_balancer_id = "${hcloud_server.okd_bootstrap[count.index].id}"
+  ip_address       = "${hcloud_server.okd_bootstrap[count.index].ipv4_address}"
   dns_ptr          = "okd-control-${count.index}.${var.cluster_name}.${base_domain}"
 }
 
@@ -79,7 +79,7 @@ resource "cloudflare_record" "dns_a_workers" {
 
   zone_id = var.cloudflare_dns_zone_id
   name    = "okd-worker-${count.index}.${var.cluster_name}.${base_domain}"
-  value   = "hcloud_server.okd_worker.${count.index}.ipv4_address"
+  value   = "${hcloud_server.okd_worker.count.index.ipv4_address}"
   type    = "A"
   ttl     = 120
 }
@@ -87,7 +87,7 @@ resource "cloudflare_record" "dns_a_workers" {
 resource "hcloud_rdns" "workers" {
   count = var.num_okd_workers
 
-  load_balancer_id = "hcloud_server.okd_worker.${count.index}.id"
-  ip_address       = "hcloud_server.okd_worker.${count.index}.ipv4_address"
+  load_balancer_id = "${hcloud_server.okd_worker[count.index].id}"
+  ip_address       = "${hcloud_server.okd_worker[count.index].ipv4_address}"
   dns_ptr          = "okd-worker-${count.index}.${var.cluster_name}.${base_domain}"
 }
